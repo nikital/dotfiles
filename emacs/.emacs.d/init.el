a70da96c4c2fce007842e09723f5609802716f32
Nikita Leshenko <nikita@leshenko.net>
2022-05-30 13:39:26 +0300

    emacs: consult: Fix call following upgrade

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -463,7 +463,7 @@ run the attached function (if exists) and enable lsp"
               "fd")))
     (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
                  (`(,re . ,hl) (funcall consult--regexp-compiler
-                                        arg 'extended)))
+                                        arg 'extended t)))
       (when re
         (list :command (append
                         (list consult--fd-command
