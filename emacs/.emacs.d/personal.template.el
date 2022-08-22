351e90614b8b63991fb9ea0cf66158d149b78731
Nikita Leshenko <nikita@leshenko.net>
2022-08-22 14:38:37 +0300

    emacs: gn goodies

--- emacs/.emacs.d/personal.template.el
+++ emacs/.emacs.d/personal.template.el
@@ -13,3 +13,29 @@
  "<" #'nik/grep-XXXXXX
  ">" #'nik/find-XXXXXX
  )
+
+(load-file "XXX/src/tools/emacs/gn.el")
+
+(defun nik/--gn-refs--get-target (f)
+  (let ((dir (string-trim-right (file-name-directory f) "/"))
+        (file (file-name-nondirectory f))
+        )
+    (cond
+     ((equal file "BUILD.gn")
+      (read-string "gn refs: " (concat dir ":" (current-word))))
+     (t f))))
+
+
+(defun nik/gn-refs ()
+  (interactive)
+  (async-shell-command
+   (concat
+    "cd XXXX && gn refs XXXX '"
+    (nik/--gn-refs--get-target (buffer-file-name))
+    "'")) )
+
+(general-define-key
+ :keymaps 'nik/spc
+ :prefix "i"
+ "r" #'nik/gn-refs
+ )
